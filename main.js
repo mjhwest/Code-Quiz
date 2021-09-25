e0d2d1f0973b3adb0d53295f17d9567cecb70e5a
mjhwest89 <mjhwest89@gmail.com>
2021-09-25 11:45:16 +0930

    start of js

---
+++ main.js
@@ -0,0 +1,8 @@
+// Declare the variables linked to HTML, 
+//querySelector(xxxx) allows us to work within that specific selector element i.e wrapper
+//documet applies that changes to the entire document 
+
+var wrapper = document.querySelector(".wrapper");
+var questionContainer = document.querySelector(".questionContainer");
+var timer = document.querySelector(".timer");
+var buttonStart = document.querySelector(".buttonStart")
