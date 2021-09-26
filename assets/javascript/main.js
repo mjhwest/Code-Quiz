d1f286cd42c098fdb1cf6f89d8ebcc548a8a88ca
mjhwest89 <mjhwest89@gmail.com>
2021-09-26 19:44:45 +0930

    save, many changes to JS

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,17 +10,26 @@
 //Declaring variables to target
 var currentQuestionIndex = 0
 var startButton = document.querySelector("#start-btn");
+// var nextButton = document.querySelector("#next-btn"); //this may not work
 var questionContEl = document.querySelector("#question-container");
 var questionElement = document.querySelector('#question');
-var answerButtonsElement = document.querySelector("answer-buttons");
+var answerButtonsElement = document.querySelector("#answer-button");
 var countdownTimerEl = document.querySelector("#countdown");
 
-//creating an event lister so when you click on the start button it starts quiz
+var countdown, currentQuestionIndex;
+var timeLeft = 75;
+
+//creating an event lister so when you click on the start button it starts quiz and countdown begins
 startButton.addEventListener("click", startGame)
+
+startButton.addEventListener("click", function() {
+    countdown();
+    console.log(countdown)
+});
 
 //creating a function that starts the game 
 function startGame() {
-    console.log("started")
+    // console.log("started")
     startButton.classList.add('hide') //hide the start button onces it selected as the game has started. 
     currentQuestionIndex = 0;
     questionContEl.classList.remove('hide'); //remove the hiden questions in questionContEl so they can be seen
@@ -28,42 +37,6 @@ function startGame() {
     showQuestion(currentQuestionIndex)
 }
 
-
-function showQuestion(currentQuestionIndex) {
-    questionElement.innerText = questions[currentQuestionIndex].question
-    question.answers.forEach(answer => {
-        var button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-}
-
-function setNextQuestion() {
-    showQuestion[currentQuestionIndex]
-}
-
-function selectAnswer() {}
-
-var questions = [{
-    question: 'What is 2 + 2? ',
-    answers: [
-        { text: '4', correct: true },
-        { text: '22', correct: false }
-    ]
-}]
-
-
-
-// creating an eventlister for the start button, when it is clicked on the function will enable a countdown timer .
-startButton.addEventListener("click", function() {
-    countdown();
-    console.log(countdown)
-});
 
 //setting the countdown timer to 75 seconds 
 function countdown() {
@@ -79,3 +52,123 @@ function countdown() {
         }
     }, 1000);
 }
+
+
+function showQuestion(currentQuestionIndex) {
+    questionElement.innerText = questions[currentQuestionIndex].question
+    questions[currentQuestionIndex].answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button);
+    });
+};
+
+function setNextQuestion() {
+    resetState()
+    showQuestion[currentQuestionIndex]
+}
+
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+            setStatusClass(button, button.dataset.correct)
+        }) //new here
+    if (correct) {
+        console.log("correct")
+
+    }
+
+    // nextButton.classList.remove('hide')
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Creating 4 different questions for the quizz 
+var questions = [{
+        question: "What does CSS stand for? ",
+        answers: [
+            { text: "Cool Style Science", correct: false },
+            { text: "Cascading Style Sheet", correct: true },
+            { text: "Computer Social Status", correct: false },
+            { text: "Computer System Security", correct: false }
+        ]
+    },
+    {
+        question: "How many milliseconds make up 1 second?",
+        answers: [
+            { text: "10", correct: false },
+            { text: "100.", correct: false },
+            { text: "1000", correct: true },
+            { text: "10000", correct: false }
+        ]
+    },
+    {
+        question: "Which of the following is not primitive value in JavaScript ?",
+        answers: [
+            { text: "String", correct: false },
+            { text: "Boolean.", correct: false },
+            { text: "Alert", correct: true },
+            { text: "Number", correct: false }
+        ]
+    },
+    {
+        question: "Is Javascript hard to understand?",
+        answers: [
+            { text: "No", correct: false },
+            { text: "Yes", correct: true }
+        ]
+    }
+];
