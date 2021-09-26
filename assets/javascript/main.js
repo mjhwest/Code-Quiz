f15efe5769762ea99179ff6b71cd8709104f6e87
mjhwest89 <mjhwest89@gmail.com>
2021-09-27 00:47:29 +0930

    updated

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -18,113 +18,6 @@ var countdownTimerEl = document.querySelector("#countdown");
 var countdown, currentQuestionIndex;
 var timeLeft = 75;
 
-//creating an event lister so when you click on the start button it starts quiz and countdown begins
-startButton.addEventListener("click", startGame)
-
-startButton.addEventListener("click", function() {
-    countdown();
-    console.log(countdown)
-});
-
-//creating a function that starts the game 
-function startGame() {
-    // console.log("started")
-    startButton.classList.add('hide') //hide the start button onces it selected as the game has started. 
-    currentQuestionIndex = 0;
-    questionContEl.classList.remove('hide'); //remove the hiden questions in questionContEl so they can be seen
-    countdownTimerEl.classList.remove('hide');
-    showQuestion(currentQuestionIndex);
-}
-
-
-//setting the countdown timer to 75 seconds 
-function countdown() {
-    //use 'setinterval; method to call a function to be executed every 1000 milliseconds, i.e 1 second 
-    //when time reaches 0, timeInterval is cleared and function ends. 
-    var timeInterval = setInterval(function() {
-        timeLeft--;
-        countdownTimerEl.textContent = timeLeft + " second left to complete quiz!"
-        if (timeLeft == 0) {
-            clearInterval(timeInterval);
-            countdownTimerEl.textContent = "Time's Up!"
-        }
-    }, 1000);
-}
-
-
-function showQuestion(currentQuestionIndex) {
-    questionElement.innerText = questions[currentQuestionIndex].question
-    questions[currentQuestionIndex].answers.forEach(answer => {
-        var button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button);
-    });
-};
-
-
-function selectAnswer(e) {
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.correct
-    if (correct) {
-        console.log("correct"); //working till here 
-        selectedButton.classList.add('correct');
-        if (currentQuestionIndex < (questions.length - 1)) {
-            // setTimeout(function() {
-            //     currentQuestionIndex++;
-            //     reset();
-            //     showQuestion();
-            // }, 200);
-            // } else {
-            //     console.log("correct, no more questions");
-            //     setTimeout(function() {
-            //         gameEnd();
-            //     }, 200);
-        };
-    } else {
-        console.log("incorrect");
-        selectedButton.classList.add('wrong');
-        countdownTimerEl.classList.add('wrong');
-        timeLeft = timeLeft - 10;
-        // if (currentQuestionIndex < (questions.length - 1)) {
-        // setTimeout(function() {
-        //     currentQuestionIndex++;
-        //     reset();
-        //     showQuestion();
-        // }, 500);
-        // } else {
-        //     console.log("incorrect, no more questions");
-        //     setTimeout(function() {
-        //         gameEnd();
-        //     }, 500);
-    };
-
-
-}
-
-function setNextQuestion() {
-    resetState()
-    if (currentQuestionIndex < (question.length - 1)) {
-        showQuestion[currentQuestionIndex]
-    }
-}
-
-function reset() {
-    countdownTimerEl.classList.remove('wrong');
-    // if there are answers we want to remove them
-    while (answerButtonsElement.firstChild) {
-        // remove the answers
-        answerButtonsElement.removeChild
-            // check if there are still answers
-            (answerButtonsElement.firstChild)
-    }
-};
-
-
 
 
 //Creating 4 different questions for the quizz 
@@ -156,10 +49,115 @@ var questions = [{
         ]
     },
     {
-        question: "Is Javascript hard to understand?",
+        question: "Is The Quiz Finised? ",
         answers: [
             { text: "No", correct: false },
             { text: "Yes", correct: true }
         ]
     }
 ];
+
+//creating an event lister so when you click on the start button it starts quiz and countdown begins
+startButton.addEventListener("click", startGame)
+startButton.addEventListener("click", function() {
+    countdown();
+    console.log(countdown)
+});
+
+//creating a function that starts the game 
+function startGame() {
+    // console.log("started")
+    startButton.classList.add('hide') //hide the start button onces it selected as the game has started. 
+    currentQuestionIndex = 0;
+    questionContEl.classList.remove('hide'); //remove the hiden questions in questionContEl so they can be seen
+    countdownTimerEl.classList.remove('hide');
+    showQuestion(currentQuestionIndex);
+}
+
+//setting the countdown timer to 75 seconds 
+function countdown() {
+    //use 'setinterval; method to call a function to be executed every 1000 milliseconds, i.e 1 second 
+    //when time reaches 0, timeInterval is cleared and function ends. 
+    var timeInterval = setInterval(function() {
+        timeLeft--;
+        countdownTimerEl.textContent = timeLeft + " second left to complete quiz!"
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+            countdownTimerEl.textContent = "Time's Up!"
+            gameFin();
+        }
+    }, 1000);
+}
+
+function showQuestion() {
+    const question = questions[currentQuestionIndex];
+    questionElement.innerText = questions[currentQuestionIndex].question
+    questions[currentQuestionIndex].answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button);
+    });
+};
+
+function selectAnswer(e) {
+    const selectedAnswer = e.target
+    const correct = selectedAnswer.dataset.correct;
+    if (correct) {
+        console.log("correct answer"); //working till here 
+        selectedAnswer.classList.add('correct');
+        if (currentQuestionIndex < (questions.length - 1)) {
+            // setTimeout(function() {
+            //     currentQuestionIndex++;
+            //     reset();
+            //     showQuestion();
+            // }, 200);
+        } else {
+            console.log("no more questions");
+            // setTimeout(function() {
+            //     gameFin();
+            // }, 200);
+        };
+    } else {
+        console.log("incorrect");
+        selectedAnswer.classList.add('wrong');
+        countdownTimerEl.classList.add('wrong');
+        timeLeft = timeLeft - 10;
+
+        // if (currentQuestionIndex < (questions.length - 1)) {
+        // setTimeout(function() {
+        //     currentQuestionIndex++;
+        //     reset();
+        //     showQuestion();
+        // }, 500);
+        // } else {
+        //     console.log("incorrect, no more questions");
+        //     setTimeout(function() {
+        //         gameEnd();
+        //     }, 500);
+    };
+
+
+}
+
+function setNextQuestion() {
+    resetState()
+    if (currentQuestionIndex < question.length - 1) {
+        showQuestion[currentQuestionIndex]
+    }
+}
+
+function reset() {
+    countdownTimerEl.classList.remove('wrong');
+    // if there are answers we want to remove them
+    while (answerButtonsElement.firstChild) {
+        // remove the answers
+        answerButtonsElement.removeChild
+            // check if there are still answers
+            (answerButtonsElement.firstChild)
+    }
+};
